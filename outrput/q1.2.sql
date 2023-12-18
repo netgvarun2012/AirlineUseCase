8941b2d5cc0007d787c380d38376a06a9e00d795
Varun <93938450+netgvarun2012@users.noreply.github.com>
2023-12-18 14:24:52 +0800

    Add files via upload

---
+++ outrput/q1.2.sql
@@ -0,0 +1,12 @@
+-- Please find `hotel_sample_data_raw_bookings.csv` and `hotel_sample_data_raw_inventories.csv`  in the `data` folder. Use either SQL or python to aggregate the data to **hotel level** with the metrics below:
+-- > Refer to appendix for the data dictionary and description
+
+-- 1. MTD (month-to-date) total bookings (as of 2023-09-18)
+-- 2. Average daily booking
+-- 3. Average weekly booking
+-- 4. Average monthly booking
+-- 5. Second last booking date
+-- 6. Recency
+-- 7. Average transaction value
+
+-- You just need to fill in either q1.2.ipynb or q1.2sql
